5b2a70565669b4447207c6522c4331b138d7af21
MD Afridi Hossain Yasin <152080561+Moafridi24@users.noreply.github.com>
2024-01-31 21:52:10 +0600

    Add files via upload

---
+++ Multiple_table.c
@@ -0,0 +1,15 @@
+//Multiple table
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the Number : ");
+    scanf(" %d",&n);
+
+    for (int i = 1; i <11; i++)
+    {
+        printf("%d X %d = %d\n",n,i,n*i);
+    }
+    
+    return 0;
+}

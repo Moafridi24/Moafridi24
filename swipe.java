12772eec3caa09bf6448dd110bc298171a28815e
MD Afridi Hossain Yasin <152080561+Moafridi24@users.noreply.github.com>
2024-02-01 14:14:17 +0600

    Add files via upload

---
+++ swipe.java
@@ -0,0 +1,13 @@
+public class swipe {
+    public static void main(String[] args) {
+        String a = "MD_Afridi",temp,b="hossain"; 
+        temp= a;
+        a=b;
+        b=temp;
+        
+        System.out.println(a+"\n"+b);
+
+
+    }
+    
+}
